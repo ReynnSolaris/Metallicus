af3275083eef1731523d67c9f29f854eeb94dca1
Aaron <busi.asammartino@gmail.com>
2019-10-02 20:42:22 -0500

    Update BotUtils.java

--- src/main/java/me/reynn/bots/metallicus/BotUtils.java
+++ src/main/java/me/reynn/bots/metallicus/BotUtils.java
@@ -1,7 +1,7 @@
 package me.reynn.bots.metallicus;
 
 public class BotUtils {
-    public static String botToken = "NTM4NTc2OTYxNzkwODAzOTY4.Dy10xA.dHKCT-oiabHMy-GcxLQ1DoySjaU";
+    public static String botToken = "";
     public static String prefix = ".";
     public static String BO_ID = "522104653790576652";
 
